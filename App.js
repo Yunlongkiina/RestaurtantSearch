073e307c48e9c0587698a7c88276f1cc5f0ac0de
Yunlongkiina <yunlong.a.liu@gmail.com>
2023-10-16 19:24:05 +0300

    swd

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+import { StyleSheet, Text, View } from 'react-native';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack'
+import SearchScreen from './src/screens/SearchScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={SearchScreen} />
+      {/* <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} /> */}
+    </Stack.Navigator>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +26,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {View, Text, StyleSheet, TextInput} from 'react-native'
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar=({term, onTermChange, ontermSubmit})=>{
+    return(
+        <View style={styles.background}>
+            <Feather name="search" style={styles.iconStyle}/>
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Search"
+                style={styles.inputStyle}
+                value={term}
+                // onChangeText={newTerm=>onTermChange(newTerm)}
+                // Same as below
+                onChangeText={onTermChange}
+                // onEndEditing={()=>ontermSubmit()}
+                // Same as below
+                onEndEditing={ontermSubmit}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor:'#F0EEEE',
+        height:50,
+        marginTop:5,
+        borderRadius:5,
+        marginHorizontal:15,
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:"center"
+    },
+    inputStyle:{
+        // borderWidth:1,
+        // borderColor:'black',
+        flex:1,
+        fontSize:20
+    },
+    iconStyle:{
+        fontSize:35,
+        alignSelf:"center"
+    }
+});
+
+export default SearchBar;
